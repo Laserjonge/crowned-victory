f3d14bc05049d9661c78b24574ad1fb52f671f08
axelder <38287158+Laserjonge@users.noreply.github.com>
2023-12-01 11:49:27 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('bm'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'data.json'
+  })
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('bl'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'data2.json'
+})
