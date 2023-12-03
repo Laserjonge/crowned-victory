426fcb8d6f0ea1a5ead4fba14bc0cd1ee9753b76
axelder <38287158+Laserjonge@users.noreply.github.com>
2023-12-03 22:35:43 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,25 @@
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bm'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'data.json'
-  })
+// var animation = bodymovin.loadAnimation({
+// 	container: document.getElementById('bm'),
+// 	renderer: 'svg',
+// 	loop: true,
+// 	autoplay: true,
+// 	path: 'data.json'
+//   })
 
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bl'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'data2.json'
-})
+// var animation = bodymovin.loadAnimation({
+// 	container: document.getElementById('bl'),
+// 	renderer: 'svg',
+// 	loop: true,
+// 	autoplay: true,
+// 	path: 'data2.json'
+// })
+
+function toggleAudio() {
+	var audio = document.getElementById("myAudio");
+
+	if (audio.paused) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
+}
