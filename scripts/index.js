b8703827d7f0a79dbd0109b25a9bc4e59459155e
axelder <38287158+Laserjonge@users.noreply.github.com>
2023-12-07 20:12:11 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,176 @@
-// var animation = bodymovin.loadAnimation({
-// 	container: document.getElementById('loading-screen'),
-// 	renderer: 'svg',
-// 	loop: false,
-// 	autoplay: true,
-// 	path: 'loading-screen.json'
-//   })
+var loadAnimation = bodymovin.loadAnimation({
+  container: document.getElementById("loading-screen"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+//   path: "loading.json",
+});
 
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('loading-screen'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'kick.json'
-  })
+var stillAnimation = bodymovin.loadAnimation({
+  container: document.getElementById("attack-screen"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "still.json",
+});
 
-// var animation = bodymovin.loadAnimation({
-// 	container: document.getElementById('bl'),
-// 	renderer: 'svg',
-// 	loop: true,
-// 	autoplay: true,
-// 	path: 'data2.json'
-// })
+var hammer = document.getElementById("hammer-button");
+var kick = document.getElementById("kick-button");
+var music = document.getElementById("music-button");
+
+// HAMMER FUNCTION
+function hammerfunction() {
+  // Change the animation path to "hammer.json"
+  changeAnimationPath("hammer.json");
+}
+
+// Function to change the animation path
+function changeAnimationPath(newPath) {
+  stillAnimation.destroy(); // Destroy the current animation instance
+  stillAnimation = bodymovin.loadAnimation({
+    // Load a new animation instance with the updated path
+    container: document.getElementById("attack-screen"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: newPath,
+  });
+}
+
+// KICK FUNCTION
+function kickFunction() {
+  // Change the animation path to "kick.json"
+  changeAnimationPath("kick.json");
+}
+
+// Function to change the animation path
+function changeAnimationPath(newPath) {
+  stillAnimation.destroy(); // Destroy the current animation instance
+  stillAnimation = bodymovin.loadAnimation({
+    // Load a new animation instance with the updated path
+    container: document.getElementById("attack-screen"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: newPath,
+  });
+}
+
+// MUSIC FUNCTION
+function musicFunction() {
+  // Change the animation path to "music.json"
+  changeAnimationPath("loading.json");
+}
+
+// Function to change the animation path
+function changeAnimationPath(newPath) {
+  stillAnimation.destroy(); // Destroy the current animation instance
+  stillAnimation = bodymovin.loadAnimation({
+    // Load a new animation instance with the updated path
+    container: document.getElementById("attack-screen"),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: newPath,
+  });
+}
+
+hammer.addEventListener("click", hammerfunction);
+kick.addEventListener("click", kickFunction);
+music.addEventListener("click", musicFunction);
 
 
 // MUTE FUNCTION
-function toggleAudio() {
-	var audio = document.getElementById("myAudio");
+var mute = document.getElementById("mute-button");
 
-	if (audio.paused) {
-		audio.play();
-	} else {
-		audio.pause();
-	}
+function toggleAudio() {
+  var audio = document.getElementById("myAudio");
+
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 }
 
+mute.addEventListener("click", toggleAudio);
 
 // RESET FUNCTION
-function refreshPage(){
-    window.location.reload();
-} 
+var reset = document.getElementById("reset-button");
+
+function refreshPage() {
+  window.location.reload();
+}
+
+reset.addEventListener("click", refreshPage);
+
+
+
+
+
+
+// DRAGGABLE FUNCTION
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    if (document.getElementById(elmnt.id + "Header")) {
+      document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+    } else {
+      elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      // Calculate the new position, ensuring it stays within the screen boundaries:
+      var newTop = Math.max(0, Math.min(window.innerHeight - elmnt.offsetHeight, elmnt.offsetTop - pos2));
+      var newLeft = Math.max(0, Math.min(window.innerWidth - elmnt.offsetWidth, elmnt.offsetLeft - pos1));
+
+      elmnt.style.top = newTop + "px";
+      elmnt.style.left = newLeft + "px";
+    }
+
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+
+  // Apply the dragElement function to make the header draggable
+  dragElement(document.getElementById("draggableElement"));
+
+
+
+
+  // CENTER
+  function alignHeader() {
+    var header = document.getElementById('draggableElement');
+
+    // Calculate the position to center the header
+    var topPosition = (window.innerHeight - header.offsetHeight) / 2;
+    var leftPosition = (window.innerWidth - header.offsetWidth) / 2;
+
+    // Set the top and left properties
+    // header.style.top = topPosition + 'px';
+    header.style.left = leftPosition + 'px';
+  }
+
+  // Align the header initially
+  alignHeader();
+
+  // Align the header on window resize
+  window.addEventListener('resize', alignHeader);
